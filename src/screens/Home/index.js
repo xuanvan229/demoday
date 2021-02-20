d41cb1e654c2205798ffebbb38f4d3934a78b9ca
Hong Xuan <xuanvan229@gmail.com>
2021-02-20 13:22:08 +0700

    init

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,5 +1,9 @@
+/* eslint-disable max-len */
+/* eslint-disable no-undef */
 /* eslint-disable no-use-before-define */
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
+
 // import './Home.css';
 // import './styles/index.scss'
 function Home() {
@@ -40,60 +44,62 @@ function Home() {
     ],
   );
 
-  useEffect(() => {
-    window.addEventListener('scroll', onScroll);
-    return () => {
-      window.removeEventListener('scroll', onScroll);
-    };
-  }, []);
+  // useEffect(() => {
+  //   window.addEventListener('scroll', onScroll);
+  //   return () => {
+  //     window.removeEventListener('scroll', onScroll);
+  //   };
+  // }, []);
 
-  const onScroll = () => {
-    const items = document.getElementsByClassName('time_line__item');
-    const scrollTop = window.pageYOffset;
-    const timeline = document.getElementById('app');
-    [...items].forEach((element) => {
-      const height = element.offsetHeight;
-      const min = element.offsetTop + 30;
-      const max = min + height;
-      if (scrollTop >= min && scrollTop < max) {
-        element.classList.add('time_line__item__active');
-        const url = element.getElementsByTagName('img')[0].currentSrc;
-        timeline.style.backgroundImage = `url(${url})`;
-      } else {
-        element.classList.remove('time_line__item__active');
-      }
-    });
-  };
+  return (<Redirect to='/login'/>);
 
-  return (
-    <div className="App py-10" id='app'>
-      <div className=" flex items-center flex-col">
-        <h1 className="text-5xl text-center z-10 text-white">Mustafa Kemal Atatürk</h1>
-        <h2 className="text-3xl text-center mt-4 z-10 text-white">
-        FATHER OF THE TURKS
-        </h2>
-        <div id="timeline-container">
-          <div className="time_line mt-20" id="timeline">
-            {
-              data.map((item, index) => (
-                <div key={item.url + index} className="time_line__item" data-text='FATHER OF THE TURKS'>
-                  <div>
-                    <img className="rounded-md" src={item.url} alt={item.year}/>
-                    <h3 className='text-white'>
-                      {item.year}
-                    </h3>
-                    <div className='text-white description'>
-                      {item.content}
-                    </div>
-                  </div>
-                </div>
-              ))
-            }
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  // const onScroll = () => {
+  //   const items = document.getElementsByClassName('time_line__item');
+  //   const scrollTop = window.pageYOffset;
+  //   const timeline = document.getElementById('app');
+  //   [...items].forEach((element) => {
+  //     const height = element.offsetHeight;
+  //     const min = element.offsetTop + 30;
+  //     const max = min + height;
+  //     if (scrollTop >= min && scrollTop < max) {
+  //       element.classList.add('time_line__item__active');
+  //       const url = element.getElementsByTagName('img')[0].currentSrc;
+  //       timeline.style.backgroundImage = `url(${url})`;
+  //     } else {
+  //       element.classList.remove('time_line__item__active');
+  //     }
+  //   });
+  // };
+
+  // return (
+  //   <div className="App py-10" id='app'>
+  //     <div className=" flex items-center flex-col">
+  //       <h1 className="text-5xl text-center z-10 text-white">Mustafa Kemal Atatürk</h1>
+  //       <h2 className="text-3xl text-center mt-4 z-10 text-white">
+  //       FATHER OF THE TURKS
+  //       </h2>
+  //       <div id="timeline-container">
+  //         <div className="time_line mt-20" id="timeline">
+  //           {
+  //             data.map((item, index) => (
+  //               <div key={item.url + index} className="time_line__item" data-text='FATHER OF THE TURKS'>
+  //                 <div>
+  //                   <img className="rounded-md" src={item.url} alt={item.year}/>
+  //                   <h3 className='text-white'>
+  //                     {item.year}
+  //                   </h3>
+  //                   <div className='text-white description'>
+  //                     {item.content}
+  //                   </div>
+  //                 </div>
+  //               </div>
+  //             ))
+  //           }
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 }
 
 export default Home;
